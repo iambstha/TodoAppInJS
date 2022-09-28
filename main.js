e4a68e9f5186370a472d474f8db2af8bd029c7d2
iambstha <iambstha@gmail.com>
2022-09-28 22:23:49 +0545

    Made modular function for list creation and deleting items from list

--- main.js
+++ main.js
@@ -1,36 +1,38 @@
 let text = document.getElementById('enter-text');
-
-
 let submit = document.getElementById('submit-btn')
 let arr = [];
+
 submit.onclick = (e) => {
     e.preventDefault();
     if (text.value == '') {
-
+        console.log("Enter something!")
     } else {
         arr.push(text.value)
-        console.log(arr)
-
-        let li = document.createElement('li');
-        li.appendChild(document.createTextNode(arr[arr.length - 1]))
-        let list = document.getElementById('todo-list');
-        list.appendChild(li)
-
-        let remove = document.createElement('button');
-        remove.appendChild(document.createTextNode("Remove"))
-        li.appendChild(remove);
-
-
-        remove.setAttribute('class', 'btn')
+        makeList();
         text.value = ''
-        let btn = document.querySelectorAll('.btn')
-        console.log(btn)
-        btn[btn.length - 1].onclick = (e) => {
-            console.log(btn[btn.length - 1].parentElement)
-            let r = btn[btn.length - 1].parentElement;
-            r.remove();
+    }
+}
 
-        }
+function makeList() {
+    let li = document.createElement('li');
+    li.appendChild(document.createTextNode(arr[arr.length - 1]))
+    let list = document.getElementById('todo-list');
+    list.appendChild(li)
+
+    let CLOSE = document.createElement('button');
+    CLOSE.appendChild(document.createTextNode("X"))
+    li.appendChild(CLOSE);
+    CLOSE.setAttribute('class', 'btn')
+    
+    deleteFromList();
+}
+
+function deleteFromList(){
+    let btn = document.querySelectorAll('.btn')
+    btn[btn.length - 1].onclick = (e) => {
+        let r = btn[btn.length - 1].parentElement;
+        r.remove();
+
     }
 }
 
